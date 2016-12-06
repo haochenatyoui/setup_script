d19346e7ede8bc670eb7233cb58ffd3d6c158d2e
Werror <h.chen.eng@gmail.com>
2016-12-05 22:18:14 -0500

    Removed unused example

--- devenv_setup.sh
+++ devenv_setup.sh
@@ -205,17 +205,10 @@ done < "$config_file"
 
 installation_summary
 
-#echo "installing custome scripts"
+#echo "installing custom components from github"
 #run_command cd ~
 #run_command echo $PWD
-#run_command git clone https://github.com/Werror/neat-vim.git
-#run_command echo "source ~/neat-vim/.vimrc" >> .vimrc
-#run_command git clone https://github.com/Werror/neat-bash.git
-#run_command echo "source ~/neat-bash/.bashrc_USER" >> .bashrc
 #run_command git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#run_command sudo apt-get install exuberant-ctags
-#run_command sudo apt-get install ack-grep
-#run_command sudo apt-get install cmake
 #run_command git clone https://github.com/magicmonty/bash-git-prompt.git
 #run_command echo "GIT_PROMPT_ONLY_IN_REPO=1" >> .bashrc
 #run_command echo "source ~/bash-git-prompt/gitprompt.sh" >> .bashrc
